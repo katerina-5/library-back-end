762c9d0f7d1b0a738b545c3650d2638fa0fa1efa
katerina-5 <pochta13524@gmail.com>
2019-05-04 12:32:19 +0300

    Add lib for parser with methods parseBook, parseAuthor, parseGenre, parseSerie

---
+++ libs/parser.js
@@ -0,0 +1,88 @@
+const BookParser = require('./../parser/book_parser').BookParser;
+const AuthorParser = require('./../parser/author_parser').AuthorParser;
+const GenreParser = require('./../parser/genre_parser').GenreParser;
+const SerieParser = require('./../parser/serie_parser').SerieParser;
+
+const Book = require('./../parser_models/book').Book;
+const Author = require('./../parser_models/author').Author;
+const Genre = require('./../parser_models/genre').Genre;
+const Serie = require('./../parser_models/serie').Serie;
+
+module.exports = {
+    parseBook,
+    parseAuthor,
+    parseGenre,
+    parseSerie,
+}
+
+async function parseBook(url) {
+    let bookParser = new BookParser(url);
+    await bookParser.parseAllInformationAboutBook();
+
+    let result = {
+        book: {},
+        authors: [],
+        genres: [],
+        serie: {}
+    }
+
+    let bookEntity = bookParser.Book;
+    result.book = bookEntity.json;
+    result.authors = bookParser.Authors;
+    result.genres = bookParser.Genres;
+    result.serie = bookParser.Serie;
+
+    console.log("Parsed book: " + JSON.stringify(result.book));
+
+    return result;
+}
+
+async function parseAuthor(url) {
+    let authorParser = new AuthorParser(url);
+    await authorParser.parseAllInformationAboutAuthor();
+
+    let result = {
+        author: {},
+        // genres: []
+    }
+
+    let authorEntity = authorParser.Author;
+    result.author = authorEntity.json;
+
+    console.log("Parsed author: " + JSON.stringify(result.author));
+
+    return result;
+}
+
+async function parseGenre(url) {
+    let genreParser = new GenreParser(url);
+    await genreParser.parseAllInformationAboutGenre();
+
+    let result = {
+        genre: {}
+    }
+
+    let genreEntity = genreParser.Genre;
+    result.genre = genreEntity.json;
+
+    console.log("Parsed genre: " + JSON.stringify(result.genre));
+
+    return result;
+}
+
+async function parseSerie(url) {
+    let serieParser = new SerieParser(url);
+    await serieParser.parseAllInformationAboutSerie();
+
+    let result = {
+        serie: {},
+        // authors: []
+    }
+
+    let serieEntity = serieParser.Serie;
+    result.serie = serieEntity.json;
+
+    console.log("Parsed serie: " + JSON.stringify(result.serie));
+
+    return result;
+}
