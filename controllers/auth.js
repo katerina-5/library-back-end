2990ccbdf5a2a94e6176838841cda07737702535
katerina-5 <pochta13524@gmail.com>
2019-05-09 13:36:06 +0300

    Update spaces in files

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,132 +3,132 @@ const bcrypt = require('bcrypt');
 const pool = require('./../config/postgresql').pool;
 
 module.exports = {
-  signIn,
-  signUp,
-  signInToManagerPanel
+    signIn,
+    signUp,
+    signInToManagerPanel
 };
 
 async function signIn(req, res, next) {
-  console.log('User authorization');
+    console.log('User authorization');
 
-  const login = req.body.login;
+    const login = req.body.login;
 
-  try {
-    const results = await pool.query('Select password from users where login = $1', [login]);
-    console.log(results);
+    try {
+        const results = await pool.query('Select password from users where login = $1', [login]);
+        console.log(results);
 
-    // hash password
-    // const hashPassword = await authLib.hashPassword(req.body.password, next);
-    const password = req.body.password;
+        // hash password
+        // const hashPassword = await authLib.hashPassword(req.body.password, next);
+        const password = req.body.password;
 
-    if (results.rowCount != 0) {
-      let dbPassword = results.rows[0].password;
+        if (results.rowCount != 0) {
+            let dbPassword = results.rows[0].password;
 
-      const check = await bcrypt.compare(password, dbPassword);
-      console.log(check);
+            const check = await bcrypt.compare(password, dbPassword);
+            console.log(check);
 
-      if (check) {
-        const token = authLib.createJWToken({
-          group: 'user',
-          login: login,
-          password: dbPassword
-          // ...req.body
-        });
+            if (check) {
+                const token = authLib.createJWToken({
+                    group: 'user',
+                    login: login,
+                    password: dbPassword
+                    // ...req.body
+                });
 
-        res.send({
-          auth: 'true',
-          message: '',
-          token: token
-        });
-      } else {
-        res.send({ auth: 'false', message: 'Password does not right!', token: '' });
-      }
-    } else {
-      res.send({ auth: 'false', message: 'Login is not in database!', token: '' });
+                res.send({
+                    auth: 'true',
+                    message: '',
+                    token: token
+                });
+            } else {
+                res.send({ auth: 'false', message: 'Password does not right!', token: '' });
+            }
+        } else {
+            res.send({ auth: 'false', message: 'Login is not in database!', token: '' });
+        }
+    } catch (error) {
+        console.log(error);
+        next(error);
     }
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
 }
 
 async function signUp(req, res, next) {
-  console.log('User registration');
+    console.log('User registration');
 
-  const { surname, name, login, password } = req.body;
+    const { surname, name, login, password } = req.body;
 
-  try {
-    const results = await pool.query('Select login from users where login = $1', [login]);
+    try {
+        const results = await pool.query('Select login from users where login = $1', [login]);
 
-    if (results.rowCount != 0) {
-      // login is already in database
-      res.send({ auth: 'false', message: 'Login is already in database!', token: '' });
-    } else {
-      // const hashPassword = await authLib.hashPassword(password, next);
-      const hashPassword = await bcrypt.hash(password, 10);
+        if (results.rowCount != 0) {
+            // login is already in database
+            res.send({ auth: 'false', message: 'Login is already in database!', token: '' });
+        } else {
+            // const hashPassword = await authLib.hashPassword(password, next);
+            const hashPassword = await bcrypt.hash(password, 10);
 
-      // create user
-      const createUser = await pool.query('Insert into users(login, password, nickname, last_name, first_name) values($1, $2, $1, $3, $4)',
-        [login, hashPassword, surname, name]);
+            // create user
+            const createUser = await pool.query('Insert into users(login, password, nickname, last_name, first_name) values($1, $2, $1, $3, $4)',
+                [login, hashPassword, surname, name]);
 
-      const token = authLib.createJWToken({
-        group: 'user',
-        login: login,
-        password: hashPassword
-        // ...req.body
-      });
+            const token = authLib.createJWToken({
+                group: 'user',
+                login: login,
+                password: hashPassword
+                // ...req.body
+            });
 
-      res.send({
-        auth: 'true',
-        message: '',
-        token: token
-      });
+            res.send({
+                auth: 'true',
+                message: '',
+                token: token
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        next(error);
     }
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
 }
 
 async function signInToManagerPanel(req, res, next) {
-  console.log('Admin authorization');
+    console.log('Admin authorization');
 
-  const login = req.body.login;
+    const login = req.body.login;
 
-  try {
-    const results = await pool.query('Select password from administrators where login = $1', [login]);
-    console.log(results);
+    try {
+        const results = await pool.query('Select password from administrators where login = $1', [login]);
+        console.log(results);
 
-    const password = req.body.password;
+        const password = req.body.password;
 
-    if (results.rowCount != 0) {
-      let dbPassword = results.rows[0].password;
+        if (results.rowCount != 0) {
+            let dbPassword = results.rows[0].password;
 
-      const check = await bcrypt.compare(password, dbPassword);
-      console.log(check);
+            const check = await bcrypt.compare(password, dbPassword);
+            console.log(check);
 
-      if (check) {
-        const token = authLib.createJWToken({
-          group: 'admin',
-          login: login,
-          password: dbPassword
-          // ...req.body
-        });
+            if (check) {
+                const token = authLib.createJWToken({
+                    group: 'admin',
+                    login: login,
+                    password: dbPassword
+                    // ...req.body
+                });
 
-        res.send({
-          auth: 'true',
-          admin: 'true',
-          message: '',
-          token: token
-        });
-      } else {
-        res.send({ auth: 'false', message: 'Password does not right!', token: '' });
-      }
-    } else {
-      res.send({ auth: 'false', message: 'Login is not in database!', token: '' });
+                res.send({
+                    auth: 'true',
+                    admin: 'true',
+                    message: '',
+                    token: token
+                });
+            } else {
+                res.send({ auth: 'false', message: 'Password does not right!', token: '' });
+            }
+        } else {
+            res.send({ auth: 'false', message: 'Login is not in database!', token: '' });
+        }
+    } catch (error) {
+        console.log(error);
+        next(error);
     }
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
 }
--- libs/auth.js
+++ libs/auth.js
@@ -2,37 +2,37 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 module.exports = {
-  verifyJWTToken,
-  createJWToken,
-  hashPassword
+    verifyJWTToken,
+    createJWToken,
+    hashPassword
 };
 
 function verifyJWTToken(token) {
-  return new Promise((resolve, reject) => {
-    jwt.verify(token, 'test', (err, decodedToken) => {
-      if (err || !decodedToken) {
-        return reject(err)
-      }
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, 'test', (err, decodedToken) => {
+            if (err || !decodedToken) {
+                return reject(err)
+            }
 
-      resolve(decodedToken)
+            resolve(decodedToken)
+        })
     })
-  })
 }
 
 function createJWToken(data = {}) {
-  const token = jwt.sign({
-    ...data
-  }, 'test');
+    const token = jwt.sign({
+        ...data
+    }, 'test');
 
-  return token
+    return token
 }
 
 async function hashPassword(password, next) {
-  try {
-    const salt = await bcrypt.genSalt(10);
-    return await bcrypt.hash(password, salt);
-  } catch (error) {
-    // Hashing failed error
-    next(error);
-  }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        return await bcrypt.hash(password, salt);
+    } catch (error) {
+        // Hashing failed error
+        next(error);
+    }
 }
