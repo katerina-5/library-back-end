20abb06274f3990fd2b2c5d038f200d9cb9c9061
katerina-5 <pochta13524@gmail.com>
2019-05-11 00:15:07 +0300

    Make simple search case-insensitive

--- controllers/search.js
+++ controllers/search.js
@@ -14,7 +14,7 @@ async function search_books(request, response, next) {
     const search_request = request.body.search_request;
 
     try {
-        const results = await pool.query('Select * from books where title_book like $1 or description like $1',
+        const results = await pool.query('Select * from books where lower(title_book) like lower($1) or lower(description) like lower($1)',
             ["%" + search_request + "%"]);
         console.log(results.rows);
         response.status(200).json(results.rows);
@@ -31,7 +31,7 @@ async function search_authors(request, response, next) {
     const search_request = request.body.search_request;
 
     try {
-        const results = await pool.query('Select * from authors where full_name like $1 or full_name_orig like $1',
+        const results = await pool.query('Select * from authors where lower(full_name) like lower($1) or lower(full_name_orig) like lower($1)',
             ["%" + search_request + "%"]);
         console.log(results.rows);
         response.status(200).json(results.rows);
@@ -48,7 +48,7 @@ async function search_genres(request, response, next) {
     const search_request = request.body.search_request;
 
     try {
-        const results = await pool.query('Select * from genres where title_genre like $1 or description like $1',
+        const results = await pool.query('Select * from genres where lower(title_genre) like lower($1) or lower(description) like lower($1)',
             ["%" + search_request + "%"]);
         console.log(results.rows);
         response.status(200).json(results.rows);
@@ -65,7 +65,7 @@ async function search_series(request, response, next) {
     const search_request = request.body.search_request;
 
     try {
-        const results = await pool.query('Select * from series where title_serie like $1 or description like $1',
+        const results = await pool.query('Select * from series where lower(title_serie) like lower($1) or lower(description) like lower($1)',
             ["%" + search_request + "%"]);
         console.log(results.rows);
         response.status(200).json(results.rows);
