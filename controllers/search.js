42d297b3f67d1984e0509d0cbd4efe00e84f793f
katerina-5 <pochta13524@gmail.com>
2019-06-01 12:44:19 +0300

    Add search using synonyms for books, authors, genres, series

--- controllers/search.js
+++ controllers/search.js
@@ -14,14 +14,40 @@ async function search_books(request, response, next) {
     const search_request = request.body.search_request;
 
     try {
-        const results = await pool.query(`Select * from books where lower(title_book) like lower($1) OR lower(description) like lower($1) 
-        OR to_tsvector(\'russian\', description) @@ to_tsquery(\'russian\', $2)
-        OR to_tsvector(\'russian\', title_book) @@ to_tsquery(\'russian\', $2)
-        OR id_book in (select id_book from bookhasauthor inner join authors using(id_author) where lower(authors.full_name) like lower($1) or lower(authors.full_name_orig) like lower($1))
-        OR id_book in (select id_book from bookhasgenre inner join genres using(id_genre) where lower(genres.title_genre) like lower($1))`,
-            ["%" + search_request + "%", search_request]);
-        console.log(results.rows);
-        response.status(200).json(results.rows);
+        const synonyms = await pool.query(`select distinct word from words where id_word in
+        (select first_word from synonyms where second_word = (select id_word from words where word = $1))
+        or id_word in (select second_word from synonyms where first_word = (select id_word from words where word = $1));`, [search_request]);
+
+        let words = [];
+        words.push(search_request);
+        if (synonyms.rowCount !== 0) {
+            synonyms.rows.forEach(row => {
+                words.push(row.word);
+            });
+        }
+        console.log(words);
+
+        let setBooks = new Set();
+        let results = [];
+        for (let i = 0; i < words.length; i++) {
+            let word = words[i];
+
+            const books = await pool.query(`Select * from books where title_book ilike $1 or description ilike $1
+            OR to_tsvector(\'russian\', description) @@ to_tsquery(\'russian\', $2)
+            OR to_tsvector(\'russian\', title_book) @@ to_tsquery(\'russian\', $2)
+            OR id_book in (select id_book from bookhasauthor inner join authors using(id_author) where authors.full_name ilike $1 or authors.full_name_orig ilike $1)
+            OR id_book in (select id_book from bookhasgenre inner join genres using(id_genre) where genres.title_genre ilike $1)`,
+                ["%" + word + "%", word]);
+
+            books.rows.forEach(book => {
+                if (!setBooks.has(book.id_book)) {
+                    setBooks.add(book.id_book);
+                    results.push(book);
+                }
+            });
+        }
+
+        response.status(200).json(results);
     } catch (error) {
         console.log(error.message);
         next(error);
@@ -35,13 +61,39 @@ async function search_authors(request, response, next) {
     const search_request = request.body.search_request;
 
     try {
-        const results = await pool.query(`Select * from authors where lower(full_name) like lower($1) OR lower(full_name_orig) like lower($1) 
-        OR to_tsvector(\'russian\', full_name) @@ to_tsquery(\'russian\', $2)
-        OR to_tsvector(\'russian\', full_name_orig) @@ to_tsquery(\'russian\', $2)
-        OR to_tsvector(\'english\', full_name_orig) @@ to_tsquery(\'english\', $2)`,
-            ["%" + search_request + "%", search_request]);
-        console.log(results.rows);
-        response.status(200).json(results.rows);
+        const synonyms = await pool.query(`select distinct word from words where id_word in
+        (select first_word from synonyms where second_word = (select id_word from words where word = $1))
+        or id_word in (select second_word from synonyms where first_word = (select id_word from words where word = $1));`, [search_request]);
+
+        let words = [];
+        words.push(search_request);
+        if (synonyms.rowCount !== 0) {
+            synonyms.rows.forEach(row => {
+                words.push(row.word);
+            });
+        }
+        console.log(words);
+
+        let setAuthors = new Set();
+        let results = [];
+        for (let i = 0; i < words.length; i++) {
+            let word = words[i];
+
+            const authors = await pool.query(`Select * from authors where full_name ilike $1 OR full_name_orig ilike $1
+            OR to_tsvector(\'russian\', full_name) @@ to_tsquery(\'russian\', $2)
+            OR to_tsvector(\'russian\', full_name_orig) @@ to_tsquery(\'russian\', $2)
+            OR to_tsvector(\'english\', full_name_orig) @@ to_tsquery(\'english\', $2)`,
+                ["%" + word + "%", word]);
+
+            authors.rows.forEach(author => {
+                if (!setAuthors.has(author.id_author)) {
+                    setAuthors.add(author.id_author);
+                    results.push(author);
+                }
+            });
+        }
+
+        response.status(200).json(results);
     } catch (error) {
         console.log(error.message);
         next(error);
@@ -55,12 +107,38 @@ async function search_genres(request, response, next) {
     const search_request = request.body.search_request;
 
     try {
-        const results = await pool.query(`Select * from genres where lower(title_genre) like lower($1) or lower(description) like lower($1) 
-        OR to_tsvector(\'russian\', description) @@ to_tsquery(\'russian\', $2)
-        OR to_tsvector(\'russian\', title_genre) @@ to_tsquery(\'russian\', $2)`,
-            ["%" + search_request + "%", search_request]);
-        console.log(results.rows);
-        response.status(200).json(results.rows);
+        const synonyms = await pool.query(`select distinct word from words where id_word in
+        (select first_word from synonyms where second_word = (select id_word from words where word = $1))
+        or id_word in (select second_word from synonyms where first_word = (select id_word from words where word = $1));`, [search_request]);
+
+        let words = [];
+        words.push(search_request);
+        if (synonyms.rowCount !== 0) {
+            synonyms.rows.forEach(row => {
+                words.push(row.word);
+            });
+        }
+        console.log(words);
+
+        let setGenres = new Set();
+        let results = [];
+        for (let i = 0; i < words.length; i++) {
+            let word = words[i];
+
+            const genres = await pool.query(`Select * from genres where title_genre ilike $1 or description ilike $1 
+            OR to_tsvector(\'russian\', description) @@ to_tsquery(\'russian\', $2)
+            OR to_tsvector(\'russian\', title_genre) @@ to_tsquery(\'russian\', $2)`,
+                ["%" + word + "%", word]);
+
+            genres.rows.forEach(genre => {
+                if (!setGenres.has(genre.id_genre)) {
+                    setGenres.add(genre.id_genre);
+                    results.push(genre);
+                }
+            });
+        }
+
+        response.status(200).json(results);
     } catch (error) {
         console.log(error.message);
         next(error);
@@ -74,12 +152,38 @@ async function search_series(request, response, next) {
     const search_request = request.body.search_request;
 
     try {
-        const results = await pool.query(`Select * from series where lower(title_serie) like lower($1) or lower(description) like lower($1) 
-        OR to_tsvector(\'russian\', description) @@ to_tsquery(\'russian\', $2)
-        OR to_tsvector(\'russian\', title_serie) @@ to_tsquery(\'russian\', $2)`,
-            ["%" + search_request + "%", search_request]);
-        console.log(results.rows);
-        response.status(200).json(results.rows);
+        const synonyms = await pool.query(`select distinct word from words where id_word in
+        (select first_word from synonyms where second_word = (select id_word from words where word = $1))
+        or id_word in (select second_word from synonyms where first_word = (select id_word from words where word = $1));`, [search_request]);
+
+        let words = [];
+        words.push(search_request);
+        if (synonyms.rowCount !== 0) {
+            synonyms.rows.forEach(row => {
+                words.push(row.word);
+            });
+        }
+        console.log(words);
+
+        let setSeries = new Set();
+        let results = [];
+        for (let i = 0; i < words.length; i++) {
+            let word = words[i];
+
+            const series = await pool.query(`Select * from series where lower(title_serie) like lower($1) or lower(description) like lower($1) 
+            OR to_tsvector(\'russian\', description) @@ to_tsquery(\'russian\', $2)
+            OR to_tsvector(\'russian\', title_serie) @@ to_tsquery(\'russian\', $2)`,
+                ["%" + word + "%", word]);
+
+            series.rows.forEach(serie => {
+                if (!setSeries.has(serie.id_serie)) {
+                    setSeries.add(serie.id_serie);
+                    results.push(serie);
+                }
+            });
+        }
+
+        response.status(200).json(results);
     } catch (error) {
         console.log(error.message);
         next(error);
